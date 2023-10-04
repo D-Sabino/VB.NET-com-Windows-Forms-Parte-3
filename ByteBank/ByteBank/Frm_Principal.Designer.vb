0c1e39c955140314083dc743e96a6a001db32be4
Daniel Sabino <dannisabino17@gmail.com>
2023-10-04 19:46:07 -0300

    Aula 01.02 - Criando um novo projeto

---
+++ ByteBank/ByteBank/Frm_Principal.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm_Principal
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lbl_Principal = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lbl_Principal
+        '
+        Me.lbl_Principal.AutoSize = True
+        Me.lbl_Principal.Location = New System.Drawing.Point(12, 9)
+        Me.lbl_Principal.Name = "lbl_Principal"
+        Me.lbl_Principal.Size = New System.Drawing.Size(51, 17)
+        Me.lbl_Principal.TabIndex = 0
+        Me.lbl_Principal.Text = "Label1"
+        '
+        'Frm_Principal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lbl_Principal)
+        Me.Name = "Frm_Principal"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lbl_Principal As Label
+End Class
---
+++ ByteBank/ByteBank/Frm_Principal.vb
@@ -0,0 +1,16 @@
+﻿Public Class Frm_Principal
+    Public Sub New()
+
+        ' Esta chamada é requerida pelo designer.
+        InitializeComponent()
+
+        ' Adicione qualquer inicialização após a chamada InitializeComponent().
+        Me.Text = "Principal"
+        lbl_Principal.Text = "Principal"
+
+    End Sub
+
+    Private Sub Frm_Principal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
