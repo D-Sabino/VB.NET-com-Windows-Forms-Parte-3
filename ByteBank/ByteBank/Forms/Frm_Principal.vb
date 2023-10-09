f96005a15356f53d4dfa9c08e7d1b7dff34a6fe0
Daniel Sabino <dannisabino17@gmail.com>
2023-10-09 20:10:10 -0300

    Aula 01.09 - Calculo completo da bonificação

--- ByteBank/ByteBank/Forms/Frm_Principal.vb
+++ ByteBank/ByteBank/Forms/Frm_Principal.vb
@@ -1,4 +1,5 @@
-﻿Imports ByteBank.Classes.Funcionarios
+﻿Imports ByteBank.Classes.Bibliotecas
+Imports ByteBank.Classes.Funcionarios
 
 Public Class Frm_Principal
     Public Sub New()
@@ -18,20 +19,32 @@ Public Class Frm_Principal
     End Sub
 
     Private Sub btm_Click_Click(sender As Object, e As EventArgs) Handles btm_Click.Click
+        Dim gerenciadorBonificacao As New GerenciadorBonificacao
+
         Dim Carlos As New Gerente()
         Carlos.nome = "Carlos Emidio"
         Carlos.cpf = "543.445.434-00"
         Carlos.salario = 10000
+        gerenciadorBonificacao.Registrar(Carlos)
 
         Dim Bia As New Diretor()
         Bia.nome = "Bia Antoria"
         Bia.cpf = "543.445.434-00"
         Bia.salario = 20000
+        gerenciadorBonificacao.Registrar(Bia)
 
         MsgBox("O Salario do " + Carlos.nome + " é " + Carlos.salario.ToString)
         MsgBox("A bonificação de " + Carlos.nome + " sera de " + Carlos.GetBonificacao().ToString)
 
         MsgBox("O Salario do " + Bia.nome + " é " + Bia.salario.ToString)
         MsgBox("A bonificação de " + Bia.nome + " sera de " + Bia.GetBonificacao().ToString)
+
+        Dim totalBonificacao As Double
+        totalBonificacao += Carlos.GetBonificacao
+        totalBonificacao += Bia.GetBonificacao
+        MsgBox("O valor total de bonificação a ser pago sera de " + totalBonificacao.ToString +
+               " o valor calculado pela biblioteca foi de " + gerenciadorBonificacao.getBonificacao.ToString)
+
+
     End Sub
 End Class
